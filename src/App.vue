aadbd2290a6d91ad0c709cf8b3b706a327195535
Maki Spariva <makspariva@gmail.com>
2024-10-24 14:17:11 +0200

    primervue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <HolaMundo />
+    <img alt="Vue logo" src="./assets/logo.png" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HolaMundo from "./components/HolaMundo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HolaMundo,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/HolaMundo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Mi primer proyecto de VUE!!</h1>
+  </div>
+</template>
+
+<script>
+//ESTO ES CÓDIGO JS (ESPECIAL
+//CADA COMPONENT que creemos 
+//siempre debemos exportarlo con un name
+export default {
+    name:"HolaMundo"
+}
+</script>
+
+<style>
+    h1 {color:red}
+</style>
